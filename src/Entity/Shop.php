b5a4af96b34c8f004e23751a7fb7efff7008b4a9
Roman Zagday <r.zagday@richbrains.net>
2022-10-24 21:08:02 +0300

    Add api platform

--- src/Entity/Shop.php
+++ src/Entity/Shop.php
@@ -2,10 +2,12 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\ShopRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: ShopRepository::class)]
+#[ApiResource]
 class Shop
 {
     #[ORM\Id]
