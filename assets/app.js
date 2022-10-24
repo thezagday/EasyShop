c32b23db5c9c7666b21972fcac9e5f43d3d01049
Roman Zagday <r.zagday@richbrains.net>
2022-10-24 20:48:55 +0300

    Integrate index page and webpack

---
+++ assets/app.js
@@ -0,0 +1,21 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+const $ = require('jquery');
+
+// any CSS you import will output into a single css file (app.css in this case)
+import './css/bootstrap.min.css';
+import './css/templatemo-style.css';
+import './fontawesome/css/all.min.css';
+
+import './js/plugins';
+
+$(window).on("load", function() {
+    $('body').addClass('loaded');
+});
+
+// start the Stimulus application
+// import './bootstrap';
