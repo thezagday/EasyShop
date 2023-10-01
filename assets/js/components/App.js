b60d98c7401b9db8f2b5b8ad46c086dccbf81d47
Roman Zagday <r.zagday@richbrains.net>
2023-10-01 19:05:32 +0300

    Remove unused Dijkstra component

--- assets/js/components/App.js
+++ assets/js/components/App.js
@@ -4,7 +4,6 @@ import {Route, Routes} from "react-router-dom";
 import Home from "./Home";
 import Shop from "./Shop/Shop";
 import Contact from "./Contact";
-import Dijkstra from "./Dijkstra";
 
 class App extends Component {
     render () {
@@ -15,7 +14,6 @@ class App extends Component {
                     <Route exact path="/" element={<Home />} />
                     <Route exact path="/shop/:id" element={<Shop/>} />
                     <Route path="/contact" element={<Contact/>} />
-                    <Route path="/dijkstra" element={<Dijkstra/>} />
                     {/*<Route path="*" element={<NotFound />} />*/}
                 </Routes>
             </div>
