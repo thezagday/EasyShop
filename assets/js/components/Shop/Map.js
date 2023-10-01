65d625bc68f1f96f2fa623dcb942065973e4bf4f
Roman Zagday <r.zagday@richbrains.net>
2023-10-01 18:07:10 +0300

    Add new Game image, fix icon img, add simple route

--- assets/js/components/Shop/Map.js
+++ assets/js/components/Shop/Map.js
@@ -3,18 +3,49 @@ import "leaflet";
 import L, { CRS } from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import { MapContainer, useMap } from 'react-leaflet';
+delete L.Icon.Default.prototype._getIconUrl;
 
-function SimpleImageOverlay() {
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+    iconUrl: require('leaflet/dist/images/marker-icon.png'),
+    shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+});
+
+function addTestRoute(map) {
+    let yx = L.latLng;
+    let xy = function(x, y) {
+        if (Array.isArray(x)) {    // When doing xy([x, y]);
+            return yx(x[1], x[0]);
+        }
+        return yx(y, x);  // When doing xy(x, y);
+    };
+
+    let sol      = xy(175.2, 145.0);
+    let mizar    = xy( 41.6, 130.1);
+    let kruegerZ = xy( 13.4,  56.5);
+    let deneb    = xy(218.7,   8.3);
+
+    L.marker(sol).addTo(map).bindPopup('Sol');
+    L.marker(mizar).addTo(map).bindPopup('Mizar');
+    L.marker(kruegerZ).addTo(map).bindPopup('Krueger-Z');
+    L.marker(deneb).addTo(map).bindPopup('Deneb');
+
+    let travel = L.polyline([sol, deneb, mizar, kruegerZ]).addTo(map);
+}
+
+function SimpleGameImageOverlay() {
     const map = useMap();
 
     useEffect(() => {
         const bounds = [[-26.5, -25], [1021.5, 1023]];
         const image = L.imageOverlay(
-            "https://i.imgur.com/Ion6X7C.jpg",
+            "https://leafletjs.com/examples/crs-simple/uqm_map_full.png",
             bounds
         ).addTo(map);
 
         map.fitBounds(image.getBounds());
+
+        addTestRoute(map);
     }, []);
 
     return null;
@@ -30,7 +61,7 @@ function Map() {
                 boundsOptions={{ padding: [50, 50] }}
                 style={{ height: "100vh" }}
             >
-                <SimpleImageOverlay />
+                <SimpleGameImageOverlay />
             </MapContainer>
         </div>
     )
