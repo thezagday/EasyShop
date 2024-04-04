6418a11c8bcf3ebad6b8564b4c0f128b47f7805f
Roman Zagday <r.zagday@richbrains.net>
2024-04-04 23:38:17 +0300

    Refactoring some components to the function

--- assets/js/components/App.js
+++ assets/js/components/App.js
@@ -1,24 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Navbar from "./Navbar";
 import {Route, Routes} from "react-router-dom";
 import Home from "./Home";
 import Shop from "./Shop/Shop";
 import Contact from "./Contact";
 
-class App extends Component {
-    render () {
-        return (
-            <div>
-                <Navbar />
-                <Routes>
-                    <Route exact path="/" element={<Home />} />
-                    <Route exact path="/shop/:id" element={<Shop/>} />
-                    <Route path="/contact" element={<Contact/>} />
-                    {/*<Route path="*" element={<NotFound />} />*/}
-                </Routes>
-            </div>
-        );
-    }
+export default function App() {
+    return (
+        <>
+            <Navbar />
+            <Routes>
+                <Route exact path="/" element={<Home />} />
+                <Route exact path="/shop/:id" element={<Shop />} />
+                <Route path="/contact" element={<Contact />} />
+                {/*<Route path="*" element={<NotFound />} />*/}
+            </Routes>
+        </>
+    );
 }
-
-export default App;
--- assets/js/components/Navbar.js
+++ assets/js/components/Navbar.js
@@ -1,42 +1,38 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {NavLink} from 'react-router-dom';
 
-class Navbar extends Component {
-    render () {
-        return (
-            <div>
-                <nav className="navbar navbar-expand-lg">
-                    <div className="container-fluid">
-                        <a className="navbar-brand" href="/">
-                            <i className="fas fa-film mr-2"></i>
-                            EasyShop
-                        </a>
-                        <button
-                            className="navbar-toggler"
-                            type="button"
-                            data-toggle="collapse"
-                            data-target="#navbarSupportedContent"
-                            aria-controls="navbarSupportedContent"
-                            aria-expanded="false"
-                            aria-label="Toggle navigation"
-                        >
-                            <i className="fas fa-bars"></i>
-                        </button>
-                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul className="navbar-nav ml-auto mb-2 mb-lg-0">
-                                <li className="nav-item">
-                                    <NavLink className={({isActive}) => (isActive ? "nav-link nav-link-1 active" : 'nav-link nav-link-1')} end to={"/"} aria-current="page">Магазины</NavLink>
-                                </li>
-                                <li className="nav-item">
-                                    <NavLink className={({isActive}) => (isActive ? "nav-link nav-link-1 active" : 'nav-link nav-link-1')} to={"/contact"} aria-current="page">Контакты</NavLink>
-                                </li>
-                            </ul>
-                        </div>
+export default function Navbar() {
+    return (
+        <>
+            <nav className="navbar navbar-expand-lg">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="/">
+                        <i className="fas fa-film mr-2"></i>
+                        EasyShop
+                    </a>
+                    <button
+                        className="navbar-toggler"
+                        type="button"
+                        data-toggle="collapse"
+                        data-target="#navbarSupportedContent"
+                        aria-controls="navbarSupportedContent"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
+                        <i className="fas fa-bars"></i>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav ml-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <NavLink className={({isActive}) => (isActive ? "nav-link nav-link-1 active" : 'nav-link nav-link-1')} end to={"/"} aria-current="page">Магазины</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className={({isActive}) => (isActive ? "nav-link nav-link-1 active" : 'nav-link nav-link-1')} to={"/contact"} aria-current="page">Контакты</NavLink>
+                            </li>
+                        </ul>
                     </div>
-                </nav>
-            </div>
-        );
-    }
+                </div>
+            </nav>
+        </>
+    );
 }
-
-export default Navbar;
